43f4fef5852f12485ea0e1b4136ed53e8083c173
shengwei0515 <shengwei199505@gmail.com>
2022-01-04 03:35:31 +0800

    feat: add oauth state valify

--- sealbot-host-web/src/app/pages/login-page/login-page.component.ts
+++ sealbot-host-web/src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TwichClaims, TwitchApis, TwitchAuthorizationGantParameter, TwitchAuthorizationGantResponse } from 'src/app/core/services/http/twitch.content';
+import { TwitchApis, TwitchOauthCodeFlowGetCodeParameter, TwitchOauthCodeFlowGetCodeResponse } from 'src/app/core/services/http/twitch.content';
 import { HostWebApis, AuthLoginParameter, AuthLoginResponse } from 'src/app/core/services/http/host-webapi.content';
 import { HttpService } from 'src/app/core/services/http/http.service';
 import { HttpParams } from '@angular/common/http';
@@ -8,7 +8,6 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-
 
 @Component({
   selector: 'app-login-page',
@@ -19,25 +18,13 @@ export class LoginPageComponent implements OnInit {
 
   ifShowPage: boolean = true;
 
-  // https://dev.twitch.tv/docs/authentication/getting-tokens-oidc#claims
-  authClaims: TwichClaims = {
-    id_token: {
-      email: null,
-      email_verified: null,
-    },
-    userinfo: {
-      picture: null,
-      preferred_username: null
-    }
-  }
-
   authPayload = {
     client_id: 'd6vb8ffiio5l89hdqi0oyahami1j5b',
     redirect_uri: 'http://localhost:4200/login',
     response_type: 'code',
-    scope: 'user:read:email+openid',
-    claims: new HttpParams({fromObject: this.authClaims})
-  } as TwitchAuthorizationGantParameter;
+    scope: 'user:read:email',
+  } as TwitchOauthCodeFlowGetCodeParameter;
+
 
   twichAuthLink: String = "";
 
@@ -49,17 +36,49 @@ export class LoginPageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const twitchResponse = this.route.snapshot.queryParams as TwitchAuthorizationGantResponse;
+    const twitchResponse = this.route.snapshot.queryParams as TwitchOauthCodeFlowGetCodeResponse;
     if (!twitchResponse['code']){
       // not yet login now
       this.ifShowPage = true;
-      let params = new HttpParams({fromObject: this.authPayload})
-      this.twichAuthLink = `${TwitchApis.OAUTH2_AUTHORIZE}?${params.toString()}`;
+      this.setLoginHref()
     }
     else{
       // user click login, pass data to backend to get jwt token
       this.ifShowPage = false;
-      this.getTokenByGrantCode(twitchResponse).subscribe({
+
+      // verify state to prevent CSRF
+      this.verifyState(twitchResponse)
+
+      // get token
+      this.getAccessTokenAndStoreInCookie(twitchResponse)
+    }
+  }
+
+  setOauthState() {
+    this.authPayload.state = Math.random().toString(36).slice(2);
+    this.cookie.set('_oauth_state', this.authPayload.state);
+  }
+
+  setLoginHref(){
+    this.setOauthState()
+    let params = new HttpParams({fromObject: this.authPayload})
+    this.twichAuthLink = `${TwitchApis.OAUTH2_AUTHORIZE}?${params.toString()}`;
+  }
+  
+  verifyState(twitchResponse: TwitchOauthCodeFlowGetCodeResponse) {
+    if (this.cookie.get('_oauth_state') != twitchResponse['state']){
+      this.router.navigate(['error'])
+    }
+  }
+
+  getTokenWithCodeFlow(twitchGantResponse: TwitchOauthCodeFlowGetCodeResponse): Observable<AuthLoginResponse> {
+    const queryObject: AuthLoginParameter = twitchGantResponse;
+    const queryParams = new HttpParams({fromObject: queryObject});
+    return this.http.get({url: HostWebApis.AuthLogin, params: queryParams}).pipe(map( res => res.body));
+  }
+
+  getAccessTokenAndStoreInCookie(twitchResponse: TwitchOauthCodeFlowGetCodeResponse){
+    this.getTokenWithCodeFlow(twitchResponse).subscribe({
       next: (loginRes) => {
         this.cookie.set('twitch_access_token', loginRes.access_token);
         this.cookie.set('twitch_refresh_token', loginRes.refresh_token);
@@ -69,13 +88,5 @@ export class LoginPageComponent implements OnInit {
         console.log(`login failed with error: ${err}`);
         this.router.navigate(['error'])
       }});
-    }
   }
-
-  getTokenByGrantCode(twitchGantResponse: TwitchAuthorizationGantResponse): Observable<AuthLoginResponse> {
-    const queryObject: AuthLoginParameter = twitchGantResponse;
-    const queryParams = new HttpParams({fromObject: queryObject});
-    return this.http.get({url: HostWebApis.AuthLogin, params: queryParams}).pipe(map( res => res.body));
-  }
-
 }
